1a9462a2d0c9dbc0cae2441a83c90c36e78794b4
Patkloe <52906039+Patkloe@users.noreply.github.com>
2021-04-14 07:43:46 -0500

    Update remove_occur.js

--- remove_occur.js
+++ remove_occur.js
@@ -1,16 +1,12 @@
 function remove_occur(s,r){
  var res;
 var tab = s.split(" ");
- var search = s.indexOf(r);
-     s[search] = "";
+ var search = tab.indexOf(r);
+     tab[search] = " ";
  for(var i = 0; i < tab.length; i++){
-      if(tab[i] === " "){
-        let sup = "";
-        // res = res + sup;
-      }
-      else
-       res = res + tab[i] + " ";
-   }    
+      if(tab[i] !== " ")
+            res = res + tab[i] + " ";
+ }
 return res;
 }
 remove_occur("The quick brown fox jumps over the lazy dog", 'the');
