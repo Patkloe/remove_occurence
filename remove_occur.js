718e5aad07fb797113f10504876f4ca73c3480b9
Patkloe <52906039+Patkloe@users.noreply.github.com>
2021-04-14 09:09:54 -0500

    Update remove_occur.js

--- remove_occur.js
+++ remove_occur.js
@@ -1,9 +1,9 @@
 function remove_occur(s,r){
  var res;
 var tab = s.split(" ");
- var search = tab.indexOf(r);
-     tab[search] = " ";
- for(var i = 0; i < tab.length; i++){
+ var search = tab.indexOf(r); // recuperation de l'index du second string en parametre de la function
+     tab[search] = " "; // on fait la mise a jour de ce string dans le tableau genere.
+ for(var i = 0; i < tab.length; i++){ // lecture de la table generee
       if(tab[i] !== " ")
             res = res + tab[i] + " ";
  }
